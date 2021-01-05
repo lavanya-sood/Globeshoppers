0d7ae32fe3b0db0c2a9692a78ac59031fb66c5d8
Lavanya Sood <lavanyasood09@gmail.com>
2021-01-05 20:01:37 +1100

    Fixed host

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 from lib.routes import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
